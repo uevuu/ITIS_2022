217e7b8b59f9d048e31530994d52ce6d938930d2
Amir Zigangaraev <amir.zigangaraev@gmail.com>
2022-11-05 17:02:55 +0300

    109. Async/await, codables

---
+++ 109/Networking/Networking/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  Networking
+//
+//  Created by Teacher on 29.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
