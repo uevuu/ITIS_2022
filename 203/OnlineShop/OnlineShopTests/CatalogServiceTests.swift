63c6aeb62e2df5a46e93cfcc1865e9399244ec70
uevuu <nikita18.marin@gmail.com>
2023-03-04 17:33:05 +0300

    somr

---
+++ 203/OnlineShop/OnlineShopTests/CatalogServiceTests.swift
@@ -0,0 +1,28 @@
+//
+//  LoginPresenterTests.swift
+//  OnlineShopTests
+//
+//  Created by Teacher on 25.02.2023.
+//
+
+import XCTest
+@testable import OnlineShop
+
+final class CatalogPresenterTest: XCTestCase {
+    @MainActor
+    override func setUp() {
+        catalogService = MockCatalogService()
+        catalogView = MockCatalogView()
+        presenter = CatalogPresenter()
+        presenter.view = catalogView
+    }
+
+    var catalogService: MockCatalogService!
+    var catalogView: MockCatalogView!
+    var presenter: CatalogPresenter!
+ 
+    func testObtainProducts() async {
+        let result = try! await catalogService.obtainProducts()
+        XCTAssertTrue(!result.isEmpty)
+    }
+}
